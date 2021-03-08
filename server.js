0d68e0c6dbc31ef2c747c6e1efa7c1edde9f8f8e
Fahmin Guliyev <baku.fehmin@gmail.com>
2021-03-08 05:21:21 +0400

    init

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
-const { createVerify } = require("crypto");
 const express = require("express");
 const app = express();
-const fs = require("fs");
 const multer = require("multer");
-const { TesseractWorker } = require("tesseract.js");
-const worker = new TesseractWorker();
+const Tesseract = require("tesseract.js");
 
 const storage = multer.diskStorage({
   destination: (req, res, callback) => {
     callback(null, "./uploads");
   },
   filename: (req, res, callback) => {
-    callback(null, res.file);
+    callback(null, res.originalname);
   },
 });
 
-const port = 5000 | process.env.PORT;
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.file);
+
+  try {
+    Tesseract.recognize(`uploads/${req.file.filename}`, "eng", {
+      logger: (m) => console.log(m),
+    }).then(({ data: { text } }) => {
+      return res.json({
+        message: text,
+      });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+const port = 5000 || process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
